987899a7b3f0c797663114bc9d27bf2c0228ba0c
maskedcreeper10 <hulshoffmarijn@gmail.com>
2026-08-08 15:12:01 +0200

    Update files to match new version

--- Plugininfo.cs
+++ Plugininfo.cs
@@ -4,5 +4,5 @@ internal static class PluginInfo
 {
     public const string PLUGIN_GUID = "io.github.maskedcreeper10.silkmetalost";
     public const string PLUGIN_NAME = "MetalOST";
-    public const string PLUGIN_VERSION = "1.0.0";
+    public const string PLUGIN_VERSION = "1.0.1";
 }
---
+++ Thunderstore/CHANGELOG.md
@@ -0,0 +1,3 @@
+# Version 1.0.1
+- Fixed Sinner's Road music
+- Shakra rite sequence no longer loops indefinitely
